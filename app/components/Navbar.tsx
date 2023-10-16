ad52d2d7c08ea73a57903297cd139dcbe734f2bd
miradil5656 <miradild404@gmail.com>
2023-10-16 18:07:35 +0900

    styling nav

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='py-10 px-8'>
+        <div className='flex justify-between items-center container  mx-auto'>
+            <h1 className='text-3xl font-extrabold'>work<span className='font-normal'>it</span></h1>
+            <h2 className='text-2xl font-semibold'>Apply for access</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
